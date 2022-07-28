2b9811d15492554972b4736918aed1e05e8492b5
Facundo Sánchez <facundosanchez9963@gmail.com>
2022-07-28 06:20:59 -0300

    load

--- API/index.js
+++ API/index.js
@@ -1,5 +1,7 @@
 const server = require('./src/app.js');
+require('dotenv').config();
+const PORT = process.env.PORT || 3001
 
-server.listen(3001, () => {
-    console.log('%s listening at 3001'); 
+server.listen(PORT,'0.0.0.0', () => {
+    console.log(`%s listening at ${PORT}`); 
   });
